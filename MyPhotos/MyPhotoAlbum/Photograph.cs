9607bb78b463d18b4d351b44cf20417796d05589
taolasy <phanvansy12@gmail.com>
2017-10-14 03:43:06 +0700

    MyPhotos
    Commit Phan Văn Sỹ

--- MyPhotos/MyPhotoAlbum/Photograph.cs
+++ MyPhotos/MyPhotoAlbum/Photograph.cs
@@ -3,10 +3,135 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
-namespace MyPhotoAlbum
+namespace Mainning.MyPhotoAlbum
 {
-    class Photograph
+    /// <summary>
+    /// The photograph class represents a photographic
+    /// image stored in the file system
+    /// </summary>
+    public class Photograph : IDisposable
     {
+        private string _fileName;
+        public string FileName
+        {
+            get { return this._fileName; }
+        }
+
+        private Bitmap _bitmap;
+        public Bitmap Image
+        {
+            get
+            {
+                if (_bitmap == null)
+                    _bitmap = new Bitmap(_fileName);
+                return _bitmap;
+            }
+        }
+
+        private string _caption = "";
+        public string Caption
+        {
+            get { return _caption; }
+            set
+            {
+                if (_caption != value)
+                {
+                    _caption = value;
+                    _hasChangeed = true;
+                }
+            }
+        }
+
+        private string _photographer = "";
+        public string Phôtgrapher
+        {
+            get { return _photographer; }
+            set
+            {
+                if (_photographer != value)
+                {
+                    _photographer = value;
+                    HasChanged = true;
+                }
+            }
+        }
+
+        private DateTime _dataTaken = DateTime.Now;
+        public DateTime DateTaken
+        {
+            get { return _dataTaken; }
+            set
+            {
+                if (_dataTaken != value)
+                {
+                    _dataTaken = value;
+                    HasChanged = true;
+                }
+            }
+        }
+
+        private string _notes = "";
+        public string Notes
+        {
+            get { return _notes; }
+            set
+            {
+                if (_notes != value)
+                {
+                    _notes = value;
+                    HasChanged = true;
+                }
+            }
+        }
+
+        private bool _hasChangeed = true;
+        public bool HasChanged
+        {
+            get { return _hasChangeed; }
+            set { _hasChangeed = value; }
+        }
+
+        public Photograph(string fileName)
+        {
+            _fileName = fileName;
+            _bitmap = null;
+            _caption = System.IO.Path.GetFileNameWithoutExtension(fileName);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Photograph)
+            {
+                Photograph p = (Photograph)obj;
+                return string.Equals(FileName, p.FileName, StringComparison.InvariantCultureIgnoreCase);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return FileName.ToLowerInvariant().GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return FileName;
+        }
+
+        public void ReleaseImage()
+        {
+            if (_bitmap != null)
+            {
+                _bitmap.Dispose();
+                _bitmap = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            ReleaseImage();
+        }
     }
 }
--- MyPhotos/MyPhotoAlbum/Properties/AssemblyInfo.cs
+++ MyPhotos/MyPhotoAlbum/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d2ee03e3-b612-491e-b10b-a361d14b0ccf")]
+[assembly: Guid("b10f7cba-5731-4917-b852-044ee7694396")]
 
 // Version information for an assembly consists of the following four values:
 //
